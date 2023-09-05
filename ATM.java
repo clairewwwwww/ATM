5a9e6a8ef573583d3e3362b24fddc3439d8d50d6
clairewwwwww <122925057+clairewwwwww@users.noreply.github.com>
2023-09-05 00:39:54 -0700

    openAccount method
    add email (String) + money (double) to ATM

--- ATM.java
+++ ATM.java
@@ -8,4 +8,26 @@ public class ATM
     {
         ATM = new HashMap<String, Double>();
     }
+    /*A method 'openAccount'
+    Input:
+        String: userId - of an email of the new user ID
+        double: amount - initial deposit amount
+    Behavior:
+        If the user does not exist
+            Create a new account for the user
+            Store their deposit amount 
+        If the user does exist, throws an error stating that the user already exists */
+    public void openAccount(String userID, double amount) throws Exception
+    {
+        if(!ATM.containsKey(userID))
+        {
+            ATM.put(userID, amount);
+            //System.out.print(userID + amount);
+        }
+        else
+        {
+            throw new Exception("The user already exists");
+        }
+    }
+
 }
