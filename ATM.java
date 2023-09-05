b97474dd5cf52af6c32411b5e789b29d68a3ac75
clairewwwwww <122925057+clairewwwwww@users.noreply.github.com>
2023-09-05 00:43:20 -0700

    closeAccount method + created a tester
    delete the account from ATM if its balance is zero
    throws an error if its balance is not zero
    created a tester - but didn't add anything yet

--- ATM.java
+++ ATM.java
@@ -22,11 +22,28 @@ public class ATM
         if(!ATM.containsKey(userID))
         {
             ATM.put(userID, amount);
-            //System.out.print(userID + amount);
         }
         else
         {
             throw new Exception("The user already exists");
+        }
+    }
+    /*A method 'closeAccount'
+    Input:
+        String: userId - of the account email / ID
+    Behavior:
+        If there is no balance
+            Closes the account, deleting the account entry
+        If there is a balance, throws an error mentioning the need to withdraw $$$ before closing */
+    public void closeAccount(String userID) throws Exception
+    {
+        if(ATM.get(userID) <= 0|| ATM.get(userID) == null)
+        {
+            ATM.remove(userID);
+        }
+        else
+        {
+            throw new Exception("Need to withdraw $$$ before closing");
         }
     }
 
